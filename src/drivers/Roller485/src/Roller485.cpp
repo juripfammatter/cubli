3de5c81d39486081a62cd77b1284d1d9eeca2a17
juripfammatter <juri.pfammatter@gmail.com>
2025-06-27 16:58:46 +0200

    implemented basic functionality of motor driver

---
+++ src/drivers/Roller485/src/Roller485.cpp
@@ -0,0 +1,196 @@
+//
+// Created by juri on 27.06.25.
+//
+#include "Roller485.h"
+
+static inline void intToFourBytes(int32_t value, int8_t *bytes)
+{
+	*reinterpret_cast<int32_t *>(bytes) = static_cast<int32_t>(value);
+}
+
+static inline void intToFourBytesCurrent(int32_t value, int8_t *bytes)
+{
+	intToFourBytes(value * 100, bytes);
+}
+
+static inline int32_t fourBytesToInt(const int8_t *bytes)
+{
+	return *reinterpret_cast<const int32_t *>(bytes);
+}
+
+static inline int32_t fourBytesToIntCurrent(const int8_t *bytes)
+{
+	return fourBytesToInt(bytes) / 100;
+}
+
+void floatToBytes(float s, uint8_t *d)
+{
+	union {
+		float value;
+		uint8_t bytes[4];
+	} float_union_t;
+	float_union_t.value = s;
+	memcpy(d, float_union_t.bytes, 4);
+}
+
+// TODO: add logging
+
+namespace roller485
+{
+Roller485::Roller485(uint8_t i2c_address)
+	: i2c_address(i2c_address), i2c_dev(DEVICE_DT_GET(DT_NODELABEL(i2c1)))
+{
+	if (!device_is_ready(i2c_dev)) {
+		printk("I2C device not ready\n");
+	}
+
+	printk("Roller485 initialized with I2C address: 0x%02X\n", i2c_address);
+
+	setRGBUserMode();
+	disableMotor();
+}
+
+Roller485::~Roller485() = default;
+
+int Roller485::sendAndCheck(uint8_t *data)
+{
+	int ret = i2c_write(this->i2c_dev, data, sizeof(data), this->i2c_address);
+	if (ret != 0) {
+		printk("Failed to send I2C command: %d\n", ret);
+		return ret;
+	}
+	return 0;
+}
+void Roller485::enableMotor()
+{
+	uint8_t command[2] = {MOTOR_ENABLE, 0x01};
+	if (sendAndCheck(command) == 0) {
+		motor_enabled = true;
+		setRGBColor(0, 255, 0);
+		printk("Motor enabled\n");
+	} else {
+		printk("Failed to enable motor\n");
+		// TODO: Change to error state
+	}
+}
+
+void Roller485::disableMotor()
+{
+	uint8_t command[2] = {MOTOR_ENABLE, 0x00};
+	if (sendAndCheck(command) == 0) {
+		motor_enabled = false;
+		setRGBColor(0, 0, 255);
+		printk("Motor disabled\n");
+	} else {
+		printk("Failed to disable motor\n");
+	}
+}
+
+void Roller485::setMode(Roller485::motor_mode_t mode)
+{
+	if (mode < SPEED_MODE || mode > ENCODER_MODE) {
+		printk("Invalid motor mode: %d\n", mode);
+		return;
+	}
+
+	uint8_t command[2] = {MOTOR_MODE, static_cast<uint8_t>(mode)};
+	if (sendAndCheck(command) == 0) {
+		motor_mode = mode;
+		printk("Motor mode set to %d\n", mode);
+	} else {
+		printk("Failed to set motor mode\n");
+	}
+}
+
+void Roller485::setRGBUserMode()
+{
+	uint8_t command[2] = {RGB_USER_MODE, 0x01};
+	if (sendAndCheck(command) == 0) {
+		printk("Motor mode set to user mode\n");
+	} else {
+		printk("Failed to set motor mode to user mode\n");
+	}
+}
+
+void Roller485::setRGBColor(uint8_t red, uint8_t green, uint8_t blue)
+{
+	uint8_t command[4] = {RGB_COLOR, blue, green, red}; // BGR format
+	if (sendAndCheck(command) == 0) {
+		//		printk("RGB color set to R:%d G:%d B:%d\n", red, green, blue);
+	} else {
+		printk("Failed to set RGB color\n");
+	}
+}
+void Roller485::setMaxCurrent(int32_t max_current, Roller485::motor_mode_t mode)
+{
+	uint8_t address;
+	int8_t max_current_bytes[4] = {0, 0, 0, 0};
+
+	switch (mode) {
+	case SPEED_MODE:
+		address = SPEED_MODE_MAX_CURRENT;
+		break;
+	case POSITION_MODE:
+		address = POSITION_MODE_MAX_CURRENT;
+		break;
+	case CURRENT_MODE:
+		printk("Current mode does not support setting max current\n");
+		return;
+	case ENCODER_MODE:
+		printk("Encoder mode does not support setting max current\n");
+		return;
+	default:
+		printk("Invalid motor mode for setting max current\n");
+		return;
+	}
+
+	intToFourBytesCurrent(max_current, max_current_bytes);
+
+	printk("Max current bytes: 0x%02X 0x%02X 0x%02X 0x%02X\n", max_current_bytes[0],
+	       max_current_bytes[1], max_current_bytes[2], max_current_bytes[3]);
+	uint8_t command[5] = {address, max_current_bytes[0], max_current_bytes[1],
+			      max_current_bytes[2], max_current_bytes[3]};
+
+	if (sendAndCheck(command) == 0) {
+		printk("Max current set to %d mA for mode %d\n",
+		       fourBytesToIntCurrent(max_current_bytes), mode);
+	} else {
+		printk("Failed to set max current\n");
+	}
+}
+
+void Roller485::setCurrent(int32_t current)
+{
+	int8_t current_bytes[4] = {0, 0, 0, 0};
+	//	uint8_t *test;
+	//	test = (uint8_t *)&current;
+
+	intToFourBytesCurrent(current, current_bytes);
+	//	uint8_t curr[4] = {};
+	//	floatToBytes(static_cast<float>(current), curr);
+
+	uint8_t command[5] = {CURRENT_MODE_CURRENT_TARGET, static_cast<uint8_t>(current_bytes[0]),
+			      static_cast<uint8_t>(current_bytes[1]),
+			      static_cast<uint8_t>(current_bytes[2]),
+			      static_cast<uint8_t>(current_bytes[3])};
+	//	uint8_t command[5] = {CURRENT_MODE_CURRENT_TARGET, curr[0], curr[1], curr[2],
+	// curr[3]};
+	//	uint8_t test_command[5] = {CURRENT_MODE_CURRENT_TARGET, test[0], test[1], test[2],
+	// test[3]};
+
+	printk("Current bytes: 0x%02X 0x%02X 0x%02X 0x%02X\n", command[4], command[3], command[2],
+	       command[1]);
+
+	//	uint8_t curr[4] = {}
+	//	floatToBytes(static_cast<float>(current), curr);
+	//	printk("Current bytes: 0x%02X 0x%02X 0x%02X 0x%02X\n", test[3], test[2], test[1],
+	//test[0]);
+
+	if (sendAndCheck(command) == 0) {
+		printk("Current set to %d mA\n", fourBytesToIntCurrent(current_bytes));
+	} else {
+		printk("Failed to set current\n");
+	}
+}
+
+} // namespace roller485
--- src/drivers/Roller485/src/main.cpp
+++ src/drivers/Roller485/src/main.cpp
@@ -1,70 +1,28 @@
 #include <cstdio>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <vector>
-#include <zephyr/drivers/i2c.h>
+#include "Roller485.h"
 
 int main(void)
 {
-	const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
-	if (!device_is_ready(i2c_dev)) {
-		printk("I2C device not ready\n");
-		return 0;
+	const uint8_t addr = 0x64;
+	roller485::Roller485 motor =
+		roller485::Roller485(addr); // Initialize Roller485 with I2C address 0x64
+
+	k_sleep(K_SECONDS(1));
+	//	motor.setMaxCurrent(1000, roller485::Roller485::SPEED_MODE);
+	motor.setMode(roller485::Roller485::CURRENT_MODE);
+	for (int i = 0; i < 5; i++) {
+		motor.enableMotor();
+		motor.setCurrent(100);
+		k_sleep(K_SECONDS(3));
+		motor.setCurrent(-100);
+		k_sleep(K_SECONDS(3));
+
+		motor.setCurrent(0);
+		k_sleep(K_SECONDS(2));
+
+		motor.disableMotor();
+		k_sleep(K_SECONDS(3));
 	}
-
-//	int ret = i2c_reg_read_byte(i2c_dev, 0x64, 0x00);
-//	if (ret != 0) {
-//		printk("Failed to read register: %d\n", ret);
-//		return 0;
-//	}
-		uint8_t addr = 0x64;
-	uint8_t motor_enable_cmd[2] = {0x00, 0x01}; // Addr, Command
-	uint8_t motor_disable_cmd[2] = {0x00, 0x00}; // Addr, Command
-	uint8_t rgb_user_mode_cmd[2] = {0x33, 0x01}; // Addr, Command
-	uint8_t rgb_blue_cmd[4] = {0x30, 0xFF, 0x00, 0x00}; // Addr, BGR
-	uint8_t rgb_green_cmd[4] = {0x30, 0x00, 0xFF, 0x00}; // Addr, BGR
-
-	uint8_t speed_current_cmd[5] = {0x50, 0x00, 0xF0, 0x00, 0x00}; // Addr, Command
-	uint8_t zero_speed_cmd[5] = {0x40, 0x00, 0x00, 0x00, 0x00}; // Addr, Command
-
-	uint8_t medium_speed_cmd[5] = {0x40, 0x00, 0xF0, 0x01, 0x00}; // Addr, Command
-
-	printk("setting max_current to %d\n", (speed_current_cmd[1] + (speed_current_cmd[2] << 8) + (speed_current_cmd[3] << 16) + (speed_current_cmd[4] << 24))/100);
-	printk("setting speed to %d\n", (medium_speed_cmd[1] + (medium_speed_cmd[2] << 8) + (medium_speed_cmd[3] << 16) + (medium_speed_cmd[4] << 24))/100);
-
-	int ret_speed_current = i2c_write(i2c_dev, speed_current_cmd, sizeof(speed_current_cmd), addr);
-	int ret_rgb_user_mode = i2c_write(i2c_dev, rgb_user_mode_cmd, sizeof(rgb_user_mode_cmd), addr);
-	if (ret_rgb_user_mode != 0) {
-		printk("Failed to send I2C command: %d\n", ret_rgb_user_mode);
-	}
-	while(true) {
-		int ret = i2c_write(i2c_dev, motor_disable_cmd, sizeof(motor_disable_cmd), addr);
-		if (ret != 0) {
-			printk("Failed to send I2C command: %d\n", ret);
-		}
-		int ret_rgb = i2c_write(i2c_dev, rgb_blue_cmd, sizeof(rgb_blue_cmd), addr);
-		if (ret_rgb != 0) {
-			printk("Failed to send I2C command: %d\n", ret_rgb);
-		}
-		k_sleep(K_SECONDS(1));
-
-
-		int ret2 = i2c_write(i2c_dev, motor_enable_cmd, sizeof(motor_enable_cmd), addr);
-		if (ret2 != 0) {
-			printk("Failed to send I2C command: %d\n", ret);
-		}
-		int ret2_rgb = i2c_write(i2c_dev, rgb_green_cmd, sizeof(rgb_green_cmd), addr);
-		if (ret2_rgb != 0) {
-			printk("Failed to send I2C command: %d\n", ret_rgb);
-		}
-
-		int ret_speed = i2c_write(i2c_dev, medium_speed_cmd, sizeof(medium_speed_cmd), addr);
-
-		k_sleep(K_SECONDS(5));
-		ret_speed = i2c_write(i2c_dev, zero_speed_cmd, sizeof(zero_speed_cmd), addr);
-		k_sleep(K_SECONDS(4));
-	}
-
-	return 0;
 }
-
